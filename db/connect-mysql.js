a8ed3a18f15665a1b97d4f5d31f826b7ade1d12a
Mariana Ramirez Rua <mariana.ramirezr@est.iudigital.edu.co>
2025-04-04 19:42:08 -0500

    Actualización general del proyecto

--- db/connect-mysql.js
+++ db/connect-mysql.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
+require('dotenv').config(); // Cargar variables de entorno
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', 
-    password: '', 
-    database: 'peliculas_bd' 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
